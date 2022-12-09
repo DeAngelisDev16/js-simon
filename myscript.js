500c85d2bb939758ac8122044b85750e948a886b
Shelbot77 <alessiodeangelis021@gmail.com>
2022-12-09 10:32:33 +0100

    Consegna esercizio tasto button/recap venerdì 2/12

--- myscript.js
+++ myscript.js
@@ -1,7 +1,22 @@
 const button = document.getElementById('counter');
 let score = 0;
+let stopScore = false;
 
 button.addEventListener('click', function(){
-    score++;
-    button.innerHTML= 'Play'  + score++;
+    if(!stopScore){
+        if(score < 10){
+            score++;
+            button.innerHTML= 'PLAY' + score++;
+        }else {
+            
+            setTimeout(() => {
+                stopScore = false;
+                score = 0;
+                button.innerHTML= 'PLAY' + score++;
+            }, 4000);
+                
+        }
+    }
+    
 })
+
